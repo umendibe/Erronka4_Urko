13caee5619f08a423b78ab9387b93f7ec6fe1339
Urko <ikumendibe25@lhusurbil.eus>
2026-02-23 09:21:16 +0100

    aplikazioko egitura nolakoa izango den

--- MAHAIGAINEKO_APLIKAZIOA/src/App.java
+++ MAHAIGAINEKO_APLIKAZIOA/src/App.java
@@ -1,5 +1,61 @@
+import java.sql.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int aukeraMenu = Integer.parseInt(br.readLine());
+
+        switch (aukeraMenu) {
+            case 1:
+
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+
+                break;
+
+            case 4:
+
+                break;
+
+            case 5:
+
+                break;
+
+            case 6:
+
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public void produktuakGehitu() {
+
+    }
+
+    public void fitxategiaIgo() {
+
+    }
+
+    public void produktuakEgnerati() {
+
+    }
+
+    public void produktuakZerrednatu() {
+
+    }
+
+    public void produktuakBilatu() {
+
     }
 }
