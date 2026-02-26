8e9308dca4bb5c95cafcd2bf241d1211eb00c728
Urko <ikumendibe25@lhusurbil.eus>
2026-02-27 00:14:41 +0100

    beharrezkoa ez dena borratzen

--- view_imports2.ps1
+++
@@ -1,9 +0,0 @@
-# Ver líneas 28-40 para ver los imports finales
-$content = Get-Content 'MAHAIG~1\src\ProduktuakLogika.java' -Encoding UTF8
-$lines = $content -split "`n"
-
-for ($i = 28; $i -lt 40; $i++) { 
-    if ($i -lt $lines.Count) {
-        Write-Host ("Line {0:D3}: {1}" -f $i, $lines[$i])
-    }
-}
